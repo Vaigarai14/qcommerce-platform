785966a86a312a6c6630a3fb8cf7ebe67aab3775
Vaikarai <vaikarai.thennarasu@theviswagroup.com>
2026-02-09 20:00:20 +0530

    fix: remove duplicate route and server listener in index.js

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,14 @@ app.get("/zenmart", (req, res) => {
 app.listen(3000, () => {
   console.log("starting the server testing !");
 });
+
+console.log("09-02-2026");
+
+app.get("/zenmart", (req, res) => {
+  res.send("Hello Zen Mart!");
+  console.log("Hello Zen Mart!");
+});
+
+app.listen(3000, () => {
+  console.log("starting the server testing !");
+});
